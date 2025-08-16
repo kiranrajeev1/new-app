1a87826cae4bf5625226fd81085cb50d0a191522
Kiran Rajeev K V <kiranrajeev2003@gmail.com>
2025-08-16 21:21:53 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://54.221.144.159:5001/api" : "/api",
   withCredentials: true,
 });
+
